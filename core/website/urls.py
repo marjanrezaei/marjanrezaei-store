0d27689b00998c2044711b7cbf4b51888e2602eb
marjanrezaei <rezaei.marjann@gmail.com>
2025-06-05 11:22:09 +0330

    create accounts app

--- core/website/urls.py
+++ core/website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'website'
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('contact/', views.ContactView.as_view(), name='contact'),
