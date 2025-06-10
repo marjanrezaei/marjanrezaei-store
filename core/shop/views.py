f584a9281a5ae567a715ca57b38a199cc3b0b8eb
marjanrezaei <rezaei.marjann@gmail.com>
2025-06-10 18:17:26 +0330

    search by Name & Category

--- core/shop/views.py
+++ core/shop/views.py
@@ -4,21 +4,32 @@ from django.views.generic import (
     ListView,
     DetailView,
 )
-from .models import ProductModel, ProductStatusType
+from .models import ProductModel, ProductStatusType, ProductCategoryModel
 from django.shortcuts import get_object_or_404
 
 
 class ShopProductGridView(ListView):
     template_name = "shop/product-grid.html"
     model = ProductModel
+    queryset = ProductModel.objects.filter(
+        status = ProductStatusType.publish.value
+    )
     paginate_by = 9
 
+
     def get_queryset(self):
-        return super().get_queryset().filter(status=ProductStatusType.publish.value)
+      queryset= ProductModel.objects.filter(
+          status=ProductStatusType.publish.value)
+      if search_q:=self.request.GET.get("q"):
+          queryset = queryset.filter(title__icontains=search_q)
+      if category_id:=self.request.GET.get("category_id"):
+          queryset = queryset.filter(category__id=category_id)
+      return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["total_items"] = self.get_queryset().count()
+        context["total_items"] = self.queryset.count()
+        context["categories"] = ProductCategoryModel.objects.all()
         return context
     
     
