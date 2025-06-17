5fd302d792b55afed9eaebc4b4dc23d388f831cf
marjanrezaei <rezaei.marjann@gmail.com>
2025-06-17 19:41:49 +0330

    Cart summery

--- core/cart/cart.py
+++ core/cart/cart.py
@@ -27,6 +27,15 @@ class CartSession:
     
     def get_cart_dict(self):
         return self._cart
+    
+    def get_total_quantity(self):
+        total_quantity = 0
+        for item in self._cart["items"]:
+            total_quantity += item["quantity"]
+        
+        # total_quantity = len(self._cart["items"]) # guantity for number kinds product
+        return total_quantity
+       
         
     def save(self):
         self.session.modified = True
