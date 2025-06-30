b3d0ab3a6084ebc59e55950d0edbf6b83dab2d33
marjanrezaei <rezaei.marjann@gmail.com>
2025-06-30 23:57:09 +0330

    DB urls for cart items

--- core/cart/urls.py
+++ core/cart/urls.py
@@ -4,9 +4,8 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-    path("session/add-product/", views.SessionAddProductView.as_view(), name="session-add-product"),
-    path("session/remove-product/", views.SessionRemoveProductView.as_view(), name="session-remove-product"),
-    path("session/update-product-quantity/", views.SessionUpdateProductQuantityView.as_view(), name="session-update-product-quantity"),
-    path("session/cart/summary/", views.SessionCartSummaryView.as_view(), name="session-cart-summary"),
-    
+    path("db/add-product/", views.DBAddProductView.as_view(), name="db-add-product"),
+    path("db/remove-product/", views.DBRemoveProductView.as_view(), name="db-remove-product"),
+    path("db/update-product-quantity/", views.DBUpdateProductQuantityView.as_view(), name="db-update-product-quantity"),
+    path("db/cart/summary/", views.DBCartSummaryView.as_view(), name="db-cart-summary"),
 ]
