5debc68d853cb9e6d4ab3b9645a1b11599cf06cc
marjanrezaei <rezaei.marjann@gmail.com>
2025-08-06 21:29:32 +0330

    show review summery

--- core/shop/templatetags/shop_tags.py
+++ core/shop/templatetags/shop_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.db.models import Count
+from review.models import ReviewModel, ReviewStatusType
+
 from shop.models import ProductStatusType, ProductModel, WishlistProductModel
 
 register = template.Library()
@@ -33,3 +36,34 @@ def show_similar_products(context, product):
     wishlist_items = get_wishlist_items(request.user)
 
     return {"similar_products": similar_products, "request":request, "wishlist_items":wishlist_items}
+
+
+@register.inclusion_tag("includes/review-summary.html")
+def review_summary(product):
+    reviews = ReviewModel.objects.filter(product=product, status=ReviewStatusType.accepted.value)
+    total = reviews.count()
+
+    # شمارش ستاره‌ها
+    rating_counts_raw = reviews.values("rate").annotate(count=Count("rate"))
+    rating_counts = {i: 0 for i in range(1, 6)}
+    for item in rating_counts_raw:
+        rating_counts[item["rate"]] = item["count"]
+
+    # محاسبه درصدها
+    rating_percentages = {
+        i: {
+            "count": rating_counts[i],
+            "percent": round((rating_counts[i] / total) * 100) if total > 0 else 0
+        }
+        for i in range(1, 6)
+    }
+
+    return {
+        "product": product,
+        "total_reviews": total,
+        "rating_data": rating_percentages
+    }
+    
+@register.filter
+def dict_get(d, key):
+    return d.get(int(key))
