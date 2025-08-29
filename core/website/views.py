0fd2bf1a593c8e85d5a53905c8b132ebead04876
marjanrezaei <rezaei.marjann@gmail.com>
2025-08-29 18:00:42 +0330

    Set dynamic slider for index

--- core/website/views.py
+++ core/website/views.py
@@ -11,6 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
 from .forms import NewsLetterForm, ContactForm
+from shop.models import ProductModel
 
     
 # ping
@@ -46,6 +47,11 @@ class MigrateView(View, SuperuserRequiredMixin):
 
 class IndexView(TemplateView):
     template_name = "website/index.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["object_list"] = ProductModel.objects.all()[:4]        
+        return context
 
 
 class AboutView(TemplateView):
